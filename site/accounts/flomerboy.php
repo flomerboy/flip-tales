40d1f9ac81eb9cd3d6624e1fc113b0a44b1d5a01
flomerboy <rmather@alumni.risd.edu>
2017-09-03 12:00:30 -0400

    kirby hello world

---
+++ site/accounts/flomerboy.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: flomerboy
+password: >
+  $2a$10$WXDu8zSiDIqqfdD0jDsgmuLjXPod3gXVe0f/upTmbxqfi507unKi2
+email: rmather@alumni.risd.edu
+language: en
+role: admin
+history:
+  - home
